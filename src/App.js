e69640572ada9831faca5100c56c443acc55d029
Hongbin Li <hblee12294@gmail.com>
2019-02-01 14:29:37 -0800

    feat: added foundations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, {Component} from 'react'
 
+import Header from './container/Header'
+import MainSection from './container/MainSection'
 class App extends Component {
   render() {
-    return <div className="App" />
+    return (
+      <div className="App">
+        <Header />
+        <MainSection />
+      </div>
+    )
   }
 }
 
